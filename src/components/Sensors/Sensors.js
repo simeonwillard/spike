36fb7bd3607cd623f071eb6a3768b9e74e389cff
Luke Schlangen <lukeschlangen@gmail.com>
2018-03-14 11:25:19 -0500

    make css hint clearer

--- src/components/Sensors/Sensors.js
+++ src/components/Sensors/Sensors.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import GyroscopicSensor from '../GyroscopicSensor/GyroscopicSensor';
 import IonDriveSensor from '../IonDriveSensor/IonDriveSensor';
 import FlameSensor from '../GyroscopicSensor/GyroscopicSensor'; // Your flames don't look very... flamey
+
 // This used to look so pretty! What's the problem? Did you lose your sense of style?
 
 class Sensors extends Component {
